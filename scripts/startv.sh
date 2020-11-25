bee435f924547090bfbf5217a81bdd5726ad2a7a
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-11-25 17:16:00 +0800

    Add start & stop scripts of visdom server side.

---
+++ scripts/startv.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+#
+# startv.sh
+# Copyright (C) 2020 Jason Young (杨郑鑫) <AI.Jason.Young@outlook.com>
+#
+# Distributed under terms of the WTFPL license.
+#
+
+
+export VISDOM_USERNAME="Jason"
+export VISDOM_PASSWORD="123456"
+export VISDOM_COOKIE="Guest_Visdom_Cookie"
+
+function start_visdom {
+    VISDOM_USE_ENV_CREDENTIALS=1 visdom -port 6789 -enable_login -force_new_cookie;
+}
+
+export -f start_visdom
+
+nohup bash -c start_visdom > visdom.log 2>&1 &
---
+++ scripts/stopv.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# stopv.sh
+# Copyright (C) 2020 Jason Young (杨郑鑫) <AI.Jason.Young@outlook.com>
+#
+# Distributed under terms of the WTFPL license.
+#
+
+
+ps -ef | grep -v grep | grep visdom | awk '{print $2}' | xargs kill -9
